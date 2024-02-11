a977d63925a6b03b8a480356760acea7c46457f7
Shri Om Trivedi <tshriom@gmail.com>
2024-02-11 18:46:36 +0530

    s3 bucket config added

--- build-server/script.js
+++ build-server/script.js
@@ -1,6 +1,18 @@
 const { exec } = require('child_process');
 const path = require('path');
 const fs = require('fs');
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+const mime = require('mime-types');
+
+const s3Client = new S3Client({
+  region: '',
+  credentials: {
+    accessKeyId: '',
+    secretAccessKey: '',
+  },
+});
+
+const PROJECT_ID = process.env.PROJECT_ID;
 
 async function init() {
   console.log('Executing script.js...');
@@ -16,7 +28,7 @@ async function init() {
     console.log('Error:', data.toString());
   });
 
-  out.on('close', () => {
+  out.on('close', async () => {
     console.log('Build Complete');
     const distFolderPath = path.join(__dirname, 'output', 'dist');
     const distFolderContents = fs.readdirSync(distFolderPath, {
@@ -26,7 +38,16 @@ async function init() {
     for (const filePath of distFolderContents) {
       if (fs.lstatSync(filePath).isDirectory()) continue;
 
-      
+      const command = new PutObjectCommand({
+        Bucket: '',
+        key: `__outputs/${PROJECT_ID}/${filePath}`,
+        Body: fs.createReadStream(filePath),
+        ContentType: mime.lookup(filePath),
+      });
+
+      await s3Client.send(command);
     }
+
+    console.log('Done...');
   });
 }
